aeb5c1a788afe5b206a9a4db7b62ca960f7f6c6b
Romain Ardiet <romaina@alittlemarket.fr>
2015-04-29 14:28:57 +0200

    Use OrderedDict backport in case of Python2.6 or earlier

--- cleo/formatters/output_formatter_style.py
+++ cleo/formatters/output_formatter_style.py
@@ -1,6 +1,10 @@
 # -*- coding: utf-8 -*-
 
-from collections import OrderedDict
+try:
+    from collections import OrderedDict
+except ImportError:
+    # python 2.6 or earlier, use backport
+    from ordereddict import OrderedDict
 
 
 class OutputFormatterStyle(object):
