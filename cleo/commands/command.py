f888ac5776a7ff8fd63c93f40fa2441b824c9ff3
Sébastien Eustace <sebastien.eustace@gmail.com>
2016-01-02 21:38:28 -0500

    Imroving Command class

--- cleo/commands/command.py
+++ cleo/commands/command.py
@@ -4,6 +4,9 @@ from .base_command import BaseCommand, CommandError
 from ..inputs.list_input import ListInput
 from ..parser import Parser
 from ..styles import CleoStyle
+from ..outputs import Output
+from ..questions import ChoiceQuestion
+from ..helpers import Table
 
 
 class Command(BaseCommand):
@@ -12,9 +15,19 @@ class Command(BaseCommand):
 
     description = None
 
-    signature = None
+    signature = ''
 
     help = ''
+
+    verbosity = Output.VERBOSITY_NORMAL
+
+    verbosity_map = {
+        'v': Output.VERBOSITY_VERBOSE,
+        'vv': Output.VERBOSITY_VERY_VERBOSE,
+        'vvv': Output.VERBOSITY_DEBUG,
+        'quiet': Output.VERBOSITY_QUIET,
+        'normal': Output.VERBOSITY_NORMAL
+    }
 
     def __init__(self, name=None):
         self.input = None
@@ -85,14 +98,137 @@ class Command(BaseCommand):
 
         return command.run(ListInput(options), self.output)
 
-    def line(self, text):
+    def argument(self, key=None):
+        """
+        Get the value of a command argument.
+
+        :param key: The argument name
+        :type key: str
+
+        :rtype: mixed
+        """
+        if key is None:
+            return self.input.get_arguments()
+
+        return self.input.get_argument(key)
+
+    def option(self, key=None):
+        """
+        Get the value of a command option.
+
+        :param key: The option name
+        :type key: str
+
+        :rtype: mixed
+        """
+        if key is None:
+            return self.input.get_options()
+
+        return self.input.get_option(key)
+
+    def confirm(self, question, default=False):
+        """
+        Confirm a question with the user.
+
+        :param question: The question to ask
+        :type question: str
+
+        :param default: The default value
+        :type default: bool
+
+        :rtype: bool
+        """
+        return self.output.confirm(question, default)
+
+    def ask(self, question, default=None):
+        """
+        Prompt the user for input.
+
+        :param question: The question to ask
+        :type question: str
+
+        :param default: The default value
+        :type default: str or None
+
+        :rtype: str
+        """
+        return self.output.ask(question, default)
+
+    def secret(self, question):
+        """
+        Prompt the user for input but hide the answer from the console.
+
+        :param question: The question to ask
+        :type question: str
+
+        :rtype: str
+        """
+        return self.output.ask_hidden(question)
+
+    def choice(self, question, choices, default=None, attempts=None, multiple=False):
+        """
+        Give the user a single choice from an list of answers.
+
+        :param question: The question to ask
+        :type question: str
+
+        :param choices: The available choices
+        :type choices: list
+
+        :param default: The default value
+        :type default: str or None
+
+        :param attempts: The max number of attempts
+        :type attempts: int
+
+        :param multiple: Multiselect
+        :type multiple: int
+
+        :rtype: str
+        """
+        question = ChoiceQuestion(question, choices, default)
+
+        question.max_attempts = attempts
+        question.multiselect = multiple
+
+        return self.output.ask_question(question)
+
+    def table(self, headers, rows, style='default'):
+        """
+        Format input to textual table.
+
+        :param headers: The table headers
+        :type headers: list
+
+        :param rows: The table rows
+        :type rows: list
+
+        :param style: The tbale style
+        :type style: str
+        """
+        table = Table(self.output)
+
+        table.set_style(style).set_headers(headers).set_rows(rows).render()
+
+    def line(self, text, style=None, verbosity=None):
         """
         Write a string as information output.
 
         :param text: The line to write
         :type text: str
+
+        :param style: The style of the string
+        :type style: str
+
+        :param verbosity: The verbosity
+        :type verbosity: None or int str
         """
-        self.output.writeln(text)
+        if style:
+            styled = '<%s>%s</>' % (style, text)
+        else:
+            styled = text
+
+        self.output.writeln(styled)
 
     def info(self, text):
         """
@@ -101,7 +237,7 @@ class Command(BaseCommand):
         :param text: The line to write
         :type text: str
         """
-        self.line('<info>%s</info>' % text)
+        self.line(text, 'info')
 
     def comment(self, text):
         """
@@ -110,7 +246,7 @@ class Command(BaseCommand):
         :param text: The line to write
         :type text: str
         """
-        self.line('<comment>%s</comment>' % text)
+        self.line(text, 'comment')
 
     def question(self, text):
         """
@@ -119,25 +255,43 @@ class Command(BaseCommand):
         :param text: The line to write
         :type text: str
         """
-        self.line('<question>%s</question>' % text)
+        self.line(text, 'question')
 
-    def error(self, text):
+    def error(self, text, block=False):
         """
         Write a string as error output.
 
         :param text: The line to write
         :type text: str
         """
-        self.line('<error>%s</error>' % text)
+        if block:
+            return self.output.error(text)
 
-    def argument(self, key=None):
-        if key is None:
-            return self.input.get_arguments()
+        self.line(text, 'error')
 
-        return self.input.get_argument(key)
+    def warning(self, text):
+        """
+        Write a string as warning output.
 
-    def option(self, key=None):
-        if key is None:
-            return self.input.get_options()
+        :param text: The line to write
+        :type text: str
+        """
+        self.output.warning(text)
 
-        return self.input.get_option(key)
+    def list(self, elements):
+        """
+        Write a list of elements.
+
+        :param elements: The elements to write a list for
+        :type elements: list
+        """
+        self.output.listing(elements)
+
+    def _parse_verbosity(self, level=None):
+        if level in self.verbosity_map:
+            level = self.verbosity_map[level]
+        elif not isinstance(level, int):
+            level = self.verbosity
+
+        return level
+
