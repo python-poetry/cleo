8232c07e0387d4f3cb3e6117f8c037482bc08e09
Sébastien Eustace <sebastien@eustace.io>
2017-04-21 14:55:52 -0500

    Updates setup

--- setup.py
+++ setup.py
@@ -35,7 +35,7 @@ setup(
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
         'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6'
+        'Programming Language :: Python :: 3.6',
         'Topic :: Software Development :: Libraries :: Python Modules',
     ],
 )
