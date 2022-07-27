691dfe0ba13a5f0ea8f8a7d74f9893589fa68146
Bartek Sokorski <b.sokorski@gmail.com>
2022-07-28 01:54:58 +0200

    Typing progress bar

--- cleo/io/outputs/output.py
+++ cleo/io/outputs/output.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from enum import Enum
+from enum import IntEnum
 from typing import TYPE_CHECKING
 from typing import Iterable
 
@@ -12,7 +13,7 @@ if TYPE_CHECKING:
     from cleo.io.outputs.section_output import SectionOutput
 
 
-class Verbosity(Enum):
+class Verbosity(IntEnum):
 
     QUIET: int = 16
     NORMAL: int = 32
