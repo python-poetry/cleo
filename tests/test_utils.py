37d4e48327bf01921298033336cf3df5fea641dc
danieleades <33452915+danieleades@users.noreply.github.com>
2022-09-05 15:29:34 +0100

    address type errors in 'cleo._utils' (#122)
    * address type errors in 'cleo._utils'
    
    * add 'time_format' test
    
    * fixup
    
    * Fix bug and add more test cases
    
    Co-authored-by: Bartosz Sokorski <b.sokorski@gmail.com>

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -3,6 +3,27 @@ from __future__ import annotations
 import pytest
 
 from cleo._utils import find_similar_names
+from cleo._utils import format_time
+
+
+@pytest.mark.parametrize(
+    ["input_secs", "expected"],
+    [
+        (0.1, "< 1 sec"),
+        (1.0, "1 sec"),
+        (2.0, "2 secs"),
+        (59.0, "59 secs"),
+        (60.0, "1 min"),
+        (120.0, "2 mins"),
+        (3600.0, "1 hr"),
+        (7200.0, "2 hrs"),
+        (129600.0, "1 day"),
+        (129601.0, "2 days"),
+        (700000.0, "9 days"),
+    ],
+)
+def test_format_time(input_secs: float, expected: str) -> None:
+    assert format_time(input_secs) == expected
 
 
 @pytest.mark.parametrize(
