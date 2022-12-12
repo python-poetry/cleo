a8b49eff510c31bee5224a1c44048781209f2eb4
Rémi Ferrand <remi.ferrand@cc.in2p3.fr>
2022-12-12 20:29:36 +0100

    feat: Add option to 'command with space in name' in fixtures
    * This triggers a bug in 'fish completion' script as described in https://github.com/python-poetry/poetry/issues/5929#issuecomment-1345580021
    * Also adapt bash, zsh and fish sample output

--- tests/commands/completion/fixtures/command_with_space_in_name.py
+++ tests/commands/completion/fixtures/command_with_space_in_name.py
@@ -2,9 +2,11 @@ from __future__ import annotations
 
 from cleo.commands.command import Command
 from cleo.helpers import argument
+from cleo.helpers import option
 
 
 class SpacedCommand(Command):
     name = "spaced command"
     description = "Command with space in name."
     arguments = [argument("test", description="test argument")]
+    options = [option("goodbye")]
