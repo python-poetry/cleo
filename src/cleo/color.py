238c2e123568de2cfe02476173b8affaeb3204f0
trag1c <trag1cdev@yahoo.com>
2023-07-19 02:00:09 +0200

    Reverted refactoring

--- src/cleo/color.py
+++ src/cleo/color.py
@@ -55,7 +55,7 @@ class Color:
             if option not in self.AVAILABLE_OPTIONS:
                 raise ValueError(
                     f'"{option}" is not a valid color option. '
-                    f"It must be one of {', '.join(self.AVAILABLE_OPTIONS)}"
+                    f"It must be one of {', '.join(self.AVAILABLE_OPTIONS.keys())}"
                 )
 
             self._options[option] = self.AVAILABLE_OPTIONS[option]
