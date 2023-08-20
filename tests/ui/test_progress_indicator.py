9cf35173644b560af69e3ea69bbf4835e5f2664e
Branch Vincent <branchevincent@gmail.com>
2023-08-20 12:24:46 -0700

    test: speed up test suite

--- tests/ui/test_progress_indicator.py
+++ tests/ui/test_progress_indicator.py
@@ -1,39 +1,39 @@
 from __future__ import annotations
 
-import time
-
 from typing import TYPE_CHECKING
 
 from cleo.ui.progress_indicator import ProgressIndicator
 
 
 if TYPE_CHECKING:
+    from typing import Callable
+
     from cleo.io.buffered_io import BufferedIO
 
 
-def test_default_indicator(ansi_io: BufferedIO) -> None:
+def test_default_indicator(ansi_io: BufferedIO, sleep: Callable[[float], None]) -> None:
     bar = ProgressIndicator(ansi_io)
     bar.start("Starting...")
-    time.sleep(0.101)
+    sleep(0.101)
     bar.advance()
-    time.sleep(0.101)
+    sleep(0.101)
     bar.advance()
-    time.sleep(0.101)
+    sleep(0.101)
     bar.advance()
-    time.sleep(0.101)
+    sleep(0.101)
     bar.advance()
-    time.sleep(0.101)
+    sleep(0.101)
     bar.advance()
-    time.sleep(0.101)
+    sleep(0.101)
     bar.set_message("Advancing...")
     bar.advance()
     bar.finish("Done...")
     bar.start("Starting Again...")
-    time.sleep(0.101)
+    sleep(0.101)
     bar.advance()
     bar.finish("Done Again...")
     bar.start("Starting Again...")
-    time.sleep(0.101)
+    sleep(0.101)
     bar.advance()
     bar.finish("Done Again...", reset_indicator=True)
 
@@ -68,29 +68,29 @@ def test_default_indicator(ansi_io: BufferedIO) -> None:
     assert expected == ansi_io.fetch_error()
 
 
-def test_explicit_format(ansi_io: BufferedIO) -> None:
+def test_explicit_format(ansi_io: BufferedIO, sleep: Callable[[float], None]) -> None:
     bar = ProgressIndicator(ansi_io, ProgressIndicator.NORMAL)
     bar.start("Starting...")
-    time.sleep(0.101)
+    sleep(0.101)
     bar.advance()
-    time.sleep(0.101)
+    sleep(0.101)
     bar.advance()
-    time.sleep(0.101)
+    sleep(0.101)
     bar.advance()
-    time.sleep(0.101)
+    sleep(0.101)
     bar.advance()
-    time.sleep(0.101)
+    sleep(0.101)
     bar.advance()
-    time.sleep(0.101)
+    sleep(0.101)
     bar.set_message("Advancing...")
     bar.advance()
     bar.finish("Done...")
     bar.start("Starting Again...")
-    time.sleep(0.101)
+    sleep(0.101)
     bar.advance()
     bar.finish("Done Again...")
     bar.start("Starting Again...")
-    time.sleep(0.101)
+    sleep(0.101)
     bar.advance()
     bar.finish("Done Again...", reset_indicator=True)
 
