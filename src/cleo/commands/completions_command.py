4c95675ca1322e8b23ca4f6381a88322d02ff46d
Jürn Brodersen <juern.brodersen@googlemail.com>
2023-08-26 15:18:03 +0200

    misc: Removed deprecated `-A` option from fish completion (#366)

--- src/cleo/commands/completions_command.py
+++ src/cleo/commands/completions_command.py
@@ -262,7 +262,7 @@ script. Consult your shells documentation for how to add such directives.
             cmds_opts += [
                 f"# {command_name}",
                 *[
-                    f"complete -c {script_name} -A "
+                    f"complete -c {script_name} "
                     f"-n '__fish_seen_subcommand_from {sanitize(command_name)}' "
                     f"-l {opt.name} -d '{sanitize(opt.description)}'"
                     for opt in sorted(cmd.definition.options, key=lambda o: o.name)
