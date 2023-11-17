2c277d2681379b0d0221d0056379602fbe43780c
Bartosz Sokorski <b.sokorski@gmail.com>
2023-11-17 11:46:31 +0100

    Remove crashtest (#382)

--- src/cleo/ui/exception_trace/component.py
+++ src/cleo/ui/exception_trace/component.py
@@ -10,22 +10,18 @@ import re
 import sys
 import tokenize
 
+from pathlib import Path
 from typing import TYPE_CHECKING
 from typing import ClassVar
 
-from crashtest.frame_collection import FrameCollection
-
 from cleo.formatters.formatter import Formatter
+from cleo.ui.exception_trace.frame_collection import FrameCollection
 
 
 if TYPE_CHECKING:
-    from crashtest.frame import Frame
-    from crashtest.solution_providers.solution_provider_repository import (
-        SolutionProviderRepository,
-    )
-
     from cleo.io.io import IO
     from cleo.io.outputs.output import Output
+    from cleo.ui.exception_trace.frame import Frame
 
 
 class Highlighter:
@@ -231,10 +227,8 @@ class ExceptionTrace:
     def __init__(
         self,
         exception: Exception,
-        solution_provider_repository: SolutionProviderRepository | None = None,
     ) -> None:
         self._exception = exception
-        self._solution_provider_repository = solution_provider_repository
         self._exc_info = sys.exc_info()
         self._ignore: str | None = None
 
@@ -252,10 +246,8 @@ class ExceptionTrace:
         else:
             self._render_exception(io, self._exception)
 
-        self._render_solution(io, self._exception)
-
     def _render_exception(self, io: IO | Output, exception: BaseException) -> None:
-        from crashtest.inspector import Inspector
+        from cleo.ui.exception_trace.inspector import Inspector
 
         inspector = Inspector(exception)
         if not inspector.frames:
@@ -297,31 +289,6 @@ class ExceptionTrace:
         for code_line in code_lines:
             self._render_line(io, code_line, indent=4)
 
-    def _render_solution(self, io: IO | Output, exception: Exception) -> None:
-        if self._solution_provider_repository is None:
-            return
-
-        solutions = self._solution_provider_repository.get_solutions_for_exception(
-            exception
-        )
-        symbol = "•" if io.supports_utf8() else "*"
-
-        for solution in solutions:
-            title = solution.solution_title
-            description = solution.solution_description
-            links = solution.documentation_links
-
-            description = description.replace("\n", "\n    ").strip(" ")
-
-            joined_links = ",".join(f"\n    <fg=blue>{link}</>" for link in links)
-            self._render_line(
-                io,
-                f"<fg=blue;options=bold>{symbol} </>"
-                f"<fg=default;options=bold>{title.rstrip('.')}</>:"
-                f" {description}{joined_links}",
-                True,
-            )
-
     def _render_trace(self, io: IO | Output, frames: FrameCollection) -> None:
         stack_frames = FrameCollection()
         for frame in frames:
@@ -341,7 +308,7 @@ class ExceptionTrace:
             frame_collections = stack_frames.compact()
             i = remaining_frames_length
             for collection in frame_collections:
-                if collection.is_repeated():
+                if collection.is_repeated:
                     if len(collection) > 1:
                         frames_message = f"<fg=yellow>{len(collection)}</> frames"
                     else:
@@ -359,7 +326,7 @@ class ExceptionTrace:
 
                 for frame in collection:
                     relative_file_path = self._get_relative_file_path(frame.filename)
-                    relative_file_path_parts = relative_file_path.split(os.path.sep)
+                    relative_file_path_parts = relative_file_path.split(os.sep)
                     relative_file_path = (
                         f"<fg=default;options=dark>{Formatter.escape(os.sep)}</>".join(
                             relative_file_path_parts[:-1]
@@ -411,22 +378,21 @@ class ExceptionTrace:
 
                     i -= 1
 
+    @staticmethod
     def _render_line(
-        self, io: IO | Output, line: str, new_line: bool = False, indent: int = 2
+        io: IO | Output, line: str, new_line: bool = False, indent: int = 2
     ) -> None:
         if new_line:
             io.write_line("")
 
         io.write_line(f"{indent * ' '}{line}")
 
-    def _get_relative_file_path(self, filepath: str) -> str:
-        cwd = os.getcwd()
+    @staticmethod
+    def _get_relative_file_path(filepath: str) -> str:
+        if cwd := Path.cwd():
+            filepath = filepath.replace(f"{cwd}{os.sep}", "")
 
-        if cwd:
-            filepath = filepath.replace(cwd + os.path.sep, "")
-
-        home = os.path.expanduser("~")
-        if home:
-            filepath = filepath.replace(home + os.path.sep, "~" + os.path.sep)
+        if home := Path("~").expanduser():
+            filepath = filepath.replace(f"{home}{os.sep}", f"~{os.sep}")
 
         return filepath
--- tests/ui/test_exception_trace.py
+++ tests/ui/test_exception_trace.py
@@ -8,12 +8,11 @@ import pytest
 
 from cleo.io.buffered_io import BufferedIO
 from cleo.io.outputs.output import Verbosity
-from cleo.ui.exception_trace import ExceptionTrace
+from cleo.ui.exception_trace.component import ExceptionTrace
 from tests.fixtures.exceptions import nested1
 from tests.fixtures.exceptions import nested2
 from tests.fixtures.exceptions import recursion
 from tests.fixtures.exceptions import simple
-from tests.fixtures.exceptions import solution
 
 
 def test_render_better_error_message() -> None:
@@ -51,7 +50,7 @@ def test_render_debug_better_error_message() -> None:
 
     trace.render(io)
 
-    lineno = 48
+    lineno = 47
     expected = f"""
   Stack trace:
 
@@ -85,7 +84,7 @@ def test_render_debug_better_error_message_recursion_error() -> None:
     except RecursionError as e:
         trace = ExceptionTrace(e)
 
-    lineno = 84
+    lineno = 83
     trace.render(io)
 
     expected = rf"""^
@@ -132,7 +131,7 @@ def test_render_very_verbose_better_error_message() -> None:
     expected = f"""
   Stack trace:
 
-  1  {trace._get_relative_file_path(__file__)}:126 in \
+  1  {trace._get_relative_file_path(__file__)}:125 in \
 test_render_very_verbose_better_error_message
        simple.simple_exception()
 
@@ -184,7 +183,7 @@ def test_render_can_ignore_given_files() -> None:
     trace.ignore_files_in(rf"^{re.escape(nested1.__file__)}$")
     trace.render(io)
 
-    lineno = 181
+    lineno = 180
     expected = f"""
   Stack trace:
 
@@ -222,7 +221,7 @@ def test_render_shows_ignored_files_if_in_debug_mode() -> None:
     trace.ignore_files_in(rf"^{re.escape(nested1.__file__)}$")
 
     trace.render(io)
-    lineno = 219
+    lineno = 218
     expected = f"""
   Stack trace:
 
@@ -269,90 +268,40 @@ test_render_shows_ignored_files_if_in_debug_mode
     assert io.fetch_output() == expected
 
 
-def test_render_supports_solutions() -> None:
-    from crashtest.solution_providers.solution_provider_repository import (
-        SolutionProviderRepository,
-    )
-
-    io = BufferedIO()
-
-    with pytest.raises(solution.CustomError) as e:
-        solution.call()
-
-    trace = ExceptionTrace(
-        e.value, solution_provider_repository=SolutionProviderRepository()
-    )
-
-    trace.render(io)
-
-    expected = f"""
-  CustomError
-
-  Error with solution
-
-  at {trace._get_relative_file_path(solution.__file__)}:16 in call
-       12│         return solution
-       13│ 
-       14│ 
-       15│ def call() -> None:
-    →  16│     raise CustomError("Error with solution")
-       17│ 
-
-  • Solution Title: Solution Description
-    https://example.com,
-    https://example2.com
-"""
-
-    assert io.fetch_output() == expected
-
-
 def test_render_falls_back_on_ascii_symbols() -> None:
-    from crashtest.solution_providers.solution_provider_repository import (
-        SolutionProviderRepository,
-    )
-
     io = BufferedIO(supports_utf8=False)
 
-    with pytest.raises(solution.CustomError) as e:
-        solution.call()
+    with pytest.raises(Exception) as e:
+        simple.simple_exception()
 
-    trace = ExceptionTrace(
-        e.value, solution_provider_repository=SolutionProviderRepository()
-    )
+    trace = ExceptionTrace(e.value)
 
     trace.render(io)
 
     expected = f"""
-  CustomError
+  Exception
 
-  Error with solution
+  Failed
 
-  at {trace._get_relative_file_path(solution.__file__)}:16 in call
-       12|         return solution
-       13| 
-       14| 
-       15| def call() -> None:
-    >  16|     raise CustomError("Error with solution")
-       17| 
-
-  * Solution Title: Solution Description
-    https://example.com,
-    https://example2.com
+  at {trace._get_relative_file_path(simple.__file__)}:2 in simple_exception
+        1| def simple_exception() -> None:
+    >   2|     raise Exception("Failed")
+        3| 
 """
 
     assert io.fetch_output() == expected
 
 
 def test_empty_source_file_do_not_break_highlighter() -> None:
-    from cleo.ui.exception_trace import Highlighter
+    from cleo.ui.exception_trace.component import Highlighter
 
     highlighter = Highlighter()
     highlighter.highlighted_lines("")
 
 
-def test_doctrings_are_corrrectly_rendered() -> None:
+def test_docstrings_are_correctly_rendered() -> None:
     from cleo.formatters.formatter import Formatter
-    from cleo.ui.exception_trace import Highlighter
+    from cleo.ui.exception_trace.component import Highlighter
 
     source = '''
 def test():
@@ -386,32 +335,6 @@ Failed
     assert io.fetch_output() == expected
 
 
-def test_simple_render_supports_solutions() -> None:
-    from crashtest.solution_providers.solution_provider_repository import (
-        SolutionProviderRepository,
-    )
-
-    io = BufferedIO()
-
-    with pytest.raises(solution.CustomError) as e:
-        solution.call()
-
-    trace = ExceptionTrace(
-        e.value, solution_provider_repository=SolutionProviderRepository()
-    )
-
-    trace.render(io, simple=True)
-
-    expected = """
-Error with solution
-
-  • Solution Title: Solution Description
-    https://example.com,
-    https://example2.com
-"""
-    assert io.fetch_output() == expected
-
-
 def test_simple_render_aborts_if_no_message() -> None:
     io = BufferedIO()
 
@@ -421,7 +344,7 @@ def test_simple_render_aborts_if_no_message() -> None:
     trace = ExceptionTrace(e.value)
 
     trace.render(io, simple=True)
-    lineno = 419
+    lineno = 342
 
     expected = f"""
   AssertionError
