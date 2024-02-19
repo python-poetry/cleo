03fe217e4d1e8f3ac67b19a79dc951e286964d63
Bartek Sokorski <b.sokorski@gmail.com>
2024-02-20 00:36:16 +0100

    Add dummy test

---
+++ tests/parser/test_parser.py
@@ -0,0 +1,8 @@
+from __future__ import annotations
+
+
+def test_parser():
+    # DO NOT REMOVE THIS
+    # It is a marker test, meant to fail
+    # This is to be removed when test coverage for the new parser is at least 90%
+    assert False  # noqa: B011
