5671f41a37f95379980b8ef1d46327f77a849531
Bartosz Sokorski <b.sokorski@gmail.com>
2024-03-15 16:53:47 +0100

    Merge Command and BaseCommand (#413)

--- src/cleo/commands/command.py
+++ src/cleo/commands/command.py
@@ -1,13 +1,16 @@
 from __future__ import annotations
 
+import inspect
+
 from typing import TYPE_CHECKING
 from typing import Any
 from typing import ClassVar
 from typing import ContextManager
 from typing import cast
 
-from cleo.commands.base_command import BaseCommand
+from cleo.exceptions import CleoError
 from cleo.formatters.style import Style
+from cleo.io.inputs.definition import Definition
 from cleo.io.inputs.string_input import StringInput
 from cleo.io.null_io import NullIO
 from cleo.io.outputs.output import Verbosity
@@ -17,6 +20,7 @@ from cleo.ui.table_separator import TableSeparator
 if TYPE_CHECKING:
     from typing import Literal
 
+    from cleo.application import Application
     from cleo.io.inputs.argument import Argument
     from cleo.io.inputs.option import Option
     from cleo.io.io import IO
@@ -27,16 +31,34 @@ if TYPE_CHECKING:
     from cleo.ui.table import Table
 
 
-class Command(BaseCommand):
+class Command:
     arguments: ClassVar[list[Argument]] = []
     options: ClassVar[list[Option]] = []
     aliases: ClassVar[list[str]] = []
     usages: ClassVar[list[str]] = []
-    commands: ClassVar[list[BaseCommand]] = []
+    commands: ClassVar[list[Command]] = []
+    name: str | None = None
+
+    description = ""
+
+    help = ""
+
+    enabled = True
+    hidden = False
 
     def __init__(self) -> None:
         self._io: IO = None  # type: ignore[assignment]
-        super().__init__()
+        self._definition = Definition()
+        self._full_definition: Definition | None = None
+        self._application: Application | None = None
+        self._ignore_validation_errors = False
+        self._synopsis: dict[str, str] = {}
+
+        self.configure()
+
+        for i, usage in enumerate(self.usages):
+            if self.name and not usage.startswith(self.name):
+                self.usages[i] = f"{self.name} {usage}"
 
     @property
     def io(self) -> IO:
@@ -95,6 +117,97 @@ class Command(BaseCommand):
         """
         return self._io.input.option(name)
 
+    @property
+    def application(self) -> Application | None:
+        return self._application
+
+    @property
+    def definition(self) -> Definition:
+        if self._full_definition is not None:
+            return self._full_definition
+
+        return self._definition
+
+    @property
+    def processed_help(self) -> str:
+        help_text = self.help
+        if not self.help:
+            help_text = self.description
+
+        is_single_command = self._application and self._application.is_single_command()
+
+        if self._application:
+            current_script = self._application.name
+        else:
+            current_script = inspect.stack()[-1][1]
+
+        return help_text.format(
+            command_name=self.name,
+            command_full_name=current_script
+            if is_single_command
+            else f"{current_script} {self.name}",
+            script_name=current_script,
+        )
+
+    def ignore_validation_errors(self) -> None:
+        self._ignore_validation_errors = True
+
+    def set_application(self, application: Application | None = None) -> None:
+        self._application = application
+
+        self._full_definition = None
+
+    def interact(self, io: IO) -> None:
+        """
+        Interacts with the user.
+        """
+
+    def initialize(self, io: IO) -> None:
+        pass
+
+    def run(self, io: IO) -> int:
+        self.merge_application_definition()
+
+        try:
+            io.input.bind(self.definition)
+        except CleoError:
+            if not self._ignore_validation_errors:
+                raise
+
+        self.initialize(io)
+
+        if io.is_interactive():
+            self.interact(io)
+
+        if io.input.has_argument("command") and io.input.argument("command") is None:
+            io.input.set_argument("command", self.name)
+
+        io.input.validate()
+
+        return self.execute(io) or 0
+
+    def merge_application_definition(self, merge_args: bool = True) -> None:
+        if self._application is None:
+            return
+
+        self._full_definition = Definition()
+        self._full_definition.add_options(self._definition.options)
+        self._full_definition.add_options(self._application.definition.options)
+
+        if merge_args:
+            self._full_definition.set_arguments(self._application.definition.arguments)
+            self._full_definition.add_arguments(self._definition.arguments)
+        else:
+            self._full_definition.set_arguments(self._definition.arguments)
+
+    def synopsis(self, short: bool = False) -> str:
+        key = "short" if short else "long"
+
+        if key not in self._synopsis:
+            self._synopsis[key] = f"{self.name} {self.definition.synopsis(short)}"
+
+        return self._synopsis[key]
+
     def confirm(
         self, question: str, default: bool = False, true_answer_regex: str = r"(?i)^y"
     ) -> bool:
@@ -141,7 +254,7 @@ class Command(BaseCommand):
         multiple: bool = False,
     ) -> Any:
         """
-        Give the user a single choice from an list of answers.
+        Give the user a single choice from a list of answers.
         """
         from cleo.ui.choice_question import ChoiceQuestion
 
