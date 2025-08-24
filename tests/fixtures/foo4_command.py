6ebd066d069725339d434c237be7cef5652173a5
dylan-robins <dylan.robins@gmail.com>
2025-08-24 17:41:04 +0200

    fix: Added missing type annotations

--- tests/fixtures/foo4_command.py
+++ tests/fixtures/foo4_command.py
@@ -7,7 +7,7 @@ from typing import ClassVar
 from cleo.commands.command import Command
 
 
-def log_stuff():
+def log_stuff() -> None:
     logging.debug("This is an debug log record")
     logging.info("This is an info log record")
     logging.warning("This is an warning log record")
